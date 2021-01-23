dadd9c90fe5193e65f873c47d4ae3840d453603d
Ryan Youngblood <rryb77@gmail.com>
2021-01-23 12:35:04 -0600

    Added a location provider, card, and list. Currently not rendering to DOM

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./KandyKorner.css"
+
+export const KandyKorner = () => (
+    <>
+        <h2>Kandy Korner</h2>
+        <small>Like giving candy to a baby!</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>500 Kandy Korn Way</div>
+        </address>
+    </>
+)
---
+++ src/components/location/LocationList.js
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from "react"
+import { LocationContext } from "./LocationProvider"
+import { LocationCard } from "./LocationCard"
+
+export const LocationList = () => {
+    const { locations, getLocations } = useContext(LocationContext)
+
+    useEffect(() => {
+        getLocations()
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div className="locations">
+            {
+                locations.map(location => {
+                    return <LocationCard key={location.id} location={location} />
+                })
+            }
+        </div>
+    )
+}
